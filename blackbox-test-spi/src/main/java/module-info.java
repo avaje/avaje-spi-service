5a852d72abe1c9a8db2521480a5ecbcb8e71f888
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-08-27 21:03:17 -0400

    final

--- blackbox-test-spi/src/main/java/module-info.java
+++ blackbox-test-spi/src/main/java/module-info.java
@@ -1,12 +1,14 @@
 import io.avaje.spi.test.SPIInterface;
+import io.avaje.spi.test.CommonClass2;
+
 module io.avaje.spi.blackbox {
   requires io.avaje.spi;
   requires java.compiler;
   provides SPIInterface with
-  		io.avaje.spi.test.CommonClass,
+  		io.avaje.spi.test. CommonClass,
 
-  		io.avaje.spi.test.CommonClass2;
+  		io.avaje.spi. test.CommonClass2;
   exports io.avaje.spi.test;
 
-  provides io.avaje.spi.test.SPIInterface.NestedSPIInterface with io.avaje.spi.test.CommonClass2;
+  provides io.avaje.spi.test.SPIInterface.NestedSPIInterface with CommonClass2;
  }
