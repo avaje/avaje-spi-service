25d339cdc41de70001e10549b59518c933609ee3
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-12-19 01:36:25 -0500

    format

--- blackbox-test-spi/src/main/java/module-info.java
+++ blackbox-test-spi/src/main/java/module-info.java
@@ -1,27 +1,29 @@
-
-
 module io.avaje.spi.blackbox {
   requires io.avaje.inject;
   requires static io.avaje.spi;
   requires java.compiler;
 
-  provides io.avaje.spi.test.SPIInterface with io.avaje.spi.test.CommonClass,
-  io.avaje.spi.test.CommonClass2,
-  io.avaje.spi.test.CommonClass3,
-  io.avaje.spi.test.CommonClass4.CommonClass4SPI,
-  io.avaje.spi.test.CommonClass4.CommonClass5SPI,
-  io.avaje.spi.test.CommonClass5,
-  io.avaje.spi.test.ManualSPI,
-  io.avaje.spi.test.SPIInterface.DefaultSPIInterface;
+  provides io.avaje.spi.test.SPIInterface with
+      io.avaje.spi.test.CommonClass,
+      io.avaje.spi.test.CommonClass2,
+      io.avaje.spi.test.CommonClass3,
+      io.avaje.spi.test.CommonClass4.CommonClass4SPI,
+      io.avaje.spi.test.CommonClass4.CommonClass5SPI,
+      io.avaje.spi.test.CommonClass5,
+      io.avaje.spi.test.ManualSPI,
+      io.avaje.spi.test.SPIInterface.DefaultSPIInterface;
 
   exports io.avaje.spi.test;
 
-  provides io.avaje.spi.test.SPIInterface.NestedSPIInterface with io.avaje.spi.test.CommonClass.CommonClassNested$SPIInterface, io.avaje.spi.test.CommonClass2, io.avaje.spi.test.SPIInterface.NestedSPIInterface.DefaultNested$$SPIInterface;
-
-  provides io.avaje.inject.spi.InjectExtension with io.avaje.spi.test.InjectProvider;
+  provides io.avaje.spi.test.SPIInterface.NestedSPIInterface with
+      io.avaje.spi.test.CommonClass.CommonClassNested$SPIInterface,
+      io.avaje.spi.test.CommonClass2,
+      io.avaje.spi.test.SPIInterface.NestedSPIInterface.DefaultNested$$SPIInterface;
+  provides io.avaje.inject.spi.InjectExtension with
+      io.avaje.spi.test.InjectProvider;
 
   uses io.avaje.spi.test.CommonClass;
   uses io.avaje.spi.test.SPIInterface;
   uses io.avaje.spi.test.SPIInterface.NestedSPIInterface;
   uses io.avaje.inject.spi.InjectExtension;
- }
+}
